f2b27272dee9d61bf2927744511ba40f007a142e
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-05-24 15:49:00 +0100

    Rendre n'importe quel widget accessible avec InkWell

--- lib/presentation/widgets/category_grid_item.dart
+++ lib/presentation/widgets/category_grid_item.dart
@@ -8,24 +8,30 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.55),
-            category.color.withOpacity(0.9),
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
+    return InkWell(
+      onTap: () {},
+      borderRadius: BorderRadius.circular(16),
+      splashColor: Theme.of(context).primaryColor,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.55),
+              category.color.withOpacity(0.9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
         ),
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context)
-            .textTheme
-            .titleLarge!
-            .copyWith(color: Theme.of(context).colorScheme.onBackground),
+        child: Text(
+          category.title,
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge!
+              .copyWith(color: Theme.of(context).colorScheme.onBackground),
+        ),
       ),
     );
   }
