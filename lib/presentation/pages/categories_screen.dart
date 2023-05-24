3f1585373f8b6d0a3cb86ac9921cc1dafa28d7dc
Tsenda-Djessy-Flavien <djesstsenda@gmail.com>
2023-05-24 15:23:20 +0100

    Affichage des éléments de catégorie sur un écran

--- lib/presentation/pages/categories_screen.dart
+++ lib/presentation/pages/categories_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dataSources/local/dummy_data.dart';
+import 'package:meals_app/presentation/widgets/category_grid_item.dart';
 import 'package:meals_app/utils/constants.dart';
 
 class CategoriesScreen extends StatelessWidget {
@@ -13,19 +15,19 @@ class CategoriesScreen extends StatelessWidget {
       // GridView -> si vous n'avez pas beaucoup d'elt à afficher
       // GridView.builder -> et autres si vous avez many d'elt to show
       body: GridView(
+        padding: const EdgeInsets.all(24),
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2, // deux colonnes
           childAspectRatio: 3 / 2, // taille des elt(grids)
           crossAxisSpacing: 20, // espacement horizontal colonnes
           mainAxisSpacing: 20, // espacement vertical colonnes
         ),
-        children: const [
-          Text('1', style: TextStyle(color: Colors.white)),
-          Text('2', style: TextStyle(color: Colors.white)),
-          Text('3', style: TextStyle(color: Colors.white)),
-          Text('4', style: TextStyle(color: Colors.white)),
-          Text('5', style: TextStyle(color: Colors.white)),
-          Text('6', style: TextStyle(color: Colors.white)),
+        children: [
+          // ...availableCategories
+          //     .map((category) => CategoryGridItem(category: category))
+          //     .toList(),
+          for (final category in availableCategories)
+            CategoryGridItem(category: category),
         ],
       ),
     );
---
+++ lib/presentation/widgets/category_grid_item.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/domain/models/category_model.dart';
+
+class CategoryGridItem extends StatelessWidget {
+  const CategoryGridItem({super.key, required this.category});
+
+  final CategoryModel category;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            category.color.withOpacity(0.55),
+            category.color.withOpacity(0.9),
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Text(
+        category.title,
+        style: Theme.of(context)
+            .textTheme
+            .titleLarge!
+            .copyWith(color: Theme.of(context).colorScheme.onBackground),
+      ),
+    );
+  }
+}
